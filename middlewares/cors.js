3b4cf5d262e27190f4f3c71aa127a5aafbc1cdde
Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
2026-01-19 00:35:37 -0500

    corrigiendo cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
   'http://localhost:1234',
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'organizacion-nestor-api.vercel.app'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
